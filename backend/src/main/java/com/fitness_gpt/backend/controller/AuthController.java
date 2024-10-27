d6e13f7f6226e63d3ec295dec89bb8e22262c0ca
Minghao1999 <m_sun11@u.pacific.edu>
2024-10-26 20:36:41 -0700

    add restrictions when register

--- backend/src/main/java/com/fitness_gpt/backend/controller/AuthController.java
+++ backend/src/main/java/com/fitness_gpt/backend/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.fitness_gpt.backend.controller;
 import com.fitness_gpt.backend.model.User;
 import com.fitness_gpt.backend.service.UserService;
 import com.fitness_gpt.backend.util.JwtUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +13,9 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
-    @Autowired
-    private JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
+    public User register(@Valid @RequestBody User user) {
         return userService.registerUser(
                 user.getUsername(),
                 user.getPassword(),
--- backend/src/main/java/com/fitness_gpt/backend/model/User.java
+++ backend/src/main/java/com/fitness_gpt/backend/model/User.java
@@ -1,5 +1,9 @@
 package com.fitness_gpt.backend.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -9,17 +13,31 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
+
     @Setter
     @Getter
+    @NotBlank(message = "Username cannot be blank")
     private String username;
+
     @Setter
     @Getter
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,20}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )
     private String password;
+
     @Setter
     @Getter
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
     @Setter
     @Getter
+    @NotBlank(message = "Phone cannot be blank")
     private String phone;
 
 }
